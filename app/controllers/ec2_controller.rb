127c569a9b870f21191a7ef81ed925a81a60ba32
nlawson <ogg@Ogg-book.local>
2016-08-26 11:24:52 -0700

    fixup for posgres odd behavior

--- app/controllers/ec2_controller.rb
+++ app/controllers/ec2_controller.rb
@@ -8,19 +8,18 @@ class Ec2Controller < ApplicationController
 
   def show
     if params.key? :query
-      @query = Ec2.joins(:nodemap)
+      @query = Ec2.joins(:nodemap).where("'nodemaps.instanceState' != ?",'terminated')
       @query = @query.where("nodemaps.name = ?", params[:query][:name]) if params[:query].key? :name
       @query = @query.where("nodemaps.role = ?", params[:query][:role])  if params[:query].key? :role
       @query = @query.where("nodemaps.environment = ?", params[:query][:environment]) if params[:query].key? :environment
-      @query = @query.where("instanceId = ?", params[:query][:instanceId]) if params[:query].key? :instanceId
-      @query = @query.where("nodemaps.instanceState = ?", params[:query][:instanceState]) if params[:query].key? :instanceState
+      @query = @query.where("'instanceId' = ?", params[:query][:instanceId]) if params[:query].key? :instanceId
+      @query = @query.where("'nodemaps.instanceState' = ?", params[:query][:instanceState]) if params[:query].key? :instanceState
       render :json => @query.order("nodemaps.environment").order("nodemaps.role").order("nodemaps.name").to_json(:include => :nodemap)
     else
-      render :json => Ec2.joins(:nodemap).where("nodemaps.instanceState != 'terminated'").order("nodemaps.environment").order("nodemaps.role").order("nodemaps.name").to_json(:include => :nodemap)
+      render :json => Ec2.joins(:nodemap).where("'nodemaps.instanceState' != ?",'terminated').order("nodemaps.environment").order("nodemaps.role").order("nodemaps.name").to_json(:include => :nodemap)
     end
 
   end
-
 
 
   private
